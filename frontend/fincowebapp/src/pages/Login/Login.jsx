3016b27c846b42eae28789af12a590e296518e09
Pedro <pepoterrax@gmail.com>
2024-11-25 17:47:24 -0300

    Cambio width del login y agrego link al home

--- frontend/fincowebapp/src/pages/Login/Login.jsx
+++ frontend/fincowebapp/src/pages/Login/Login.jsx
@@ -5,12 +5,11 @@ import fincoLogo from '../../assets/fincoLogo.svg'
 export const Login = () => {
     return (
       <div className="container-dark">
-        <img src={fincoLogo} alt="logo finco" className='logo' />
+        <a href="/"><img src={fincoLogo} alt="logo finco" className='logo' /></a>
         <form action="login" method="post">
             <input type="email" name="email" id="email" placeholder='Correo electronico...' />
             <input type='password' name='password' id='password' placeholder='Contraseña...'></input>            
             <input type="submit" value="Iniciar Sesión" className='btnLogin' />
-            <p>¿Aún no tienes cuenta? <a href="registro">Registrate!</a></p>
         </form>
 
       </div>
