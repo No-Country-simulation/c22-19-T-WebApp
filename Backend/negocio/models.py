1337f816baf3854d6de369254ebb9870b9b5bbd8
Ignacio Nava <ignan_90@hotmail.com>
2024-11-25 19:11:11 -0300

    fix: campos ciudad y provincia

--- Backend/negocio/models.py
+++ Backend/negocio/models.py
@@ -2,64 +2,83 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import now
 
+
 class Provincia(models.Model):
-	nombre = models.CharField(max_length=200)
-	
-	def __str__(self):
-		return self.nombre
+    nombre = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
 
 class Ciudad(models.Model):
-	nombre = models.CharField(max_length=200)
-	provincia_id = models.ForeignKey(Provincia, on_delete=models.CASCADE, null=True, blank=True)
-	def __str__(self):
-		return self.nombre
-	class Meta:
-		verbose_name_plural = "ciudades"
-	
+    nombre = models.CharField(max_length=200)
+    provincia = models.ForeignKey(
+        Provincia, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name_plural = "ciudades"
+
+
 class Sucursal(models.Model):
-	nombre = models.CharField(max_length=200)
-	ciudad_id = models.ForeignKey(Ciudad, on_delete=models.CASCADE, null=True, blank=True)
-	def __str__(self):
-		return self.nombre
-	class Meta:
-		verbose_name_plural = "sucursales"
+    nombre = models.CharField(max_length=200)
+    ciudad = models.ForeignKey(
+        Ciudad, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name_plural = "sucursales"
+
+
 class Rol(models.Model):
-	nombre = models.CharField(max_length=200)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
-	class Meta:
-		verbose_name_plural = "roles"
-	def __str__(self):
-		return self.nombre
+    nombre = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "roles"
+
+    def __str__(self):
+        return self.nombre
 
 
 class Perfil(models.Model):
-	# Relación One-to-One con el modelo User
-	user = models.OneToOneField(
-		User, on_delete=models.CASCADE, related_name="perfil")
-	# Campos adicionales
-	rol = models.ForeignKey(
-		Rol, on_delete=models.CASCADE, null=True, blank=True)
-	sucursal = models.ForeignKey(
-		Sucursal, on_delete=models.CASCADE, null=True, blank=True)
-	superior = models.ForeignKey(
-		'self', on_delete=models.SET_NULL, null=True, blank=True, related_name="subordinados"
-	)
-	# created_at = models.DateTimeField(default=now)
-	updated_at = models.DateTimeField(auto_now=True)
-	class Meta:
-		verbose_name_plural = "perfiles"
-	def __str__(self):
-		return self.user.username
-	
+    # Relación One-to-One con el modelo User
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="perfil")
+    # Campos adicionales
+    rol = models.ForeignKey(
+        Rol, on_delete=models.CASCADE, null=True, blank=True)
+    sucursal = models.ForeignKey(
+        Sucursal, on_delete=models.CASCADE, null=True, blank=True)
+    superior = models.ForeignKey(
+        'self', on_delete=models.SET_NULL, null=True, blank=True, related_name="subordinados"
+    )
+    # created_at = models.DateTimeField(default=now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "perfiles"
+
+    def __str__(self):
+        return self.user.username
+
+
 class Venta(models.Model):
-	perfil = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-	total = models.DecimalField(max_digits=12, decimal_places=2)
-	fecha = models.DateTimeField(auto_now_add=True)
+    perfil = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    total = models.DecimalField(max_digits=12, decimal_places=2)
+    fecha = models.DateTimeField(auto_now_add=True)
+
 
 class Meta(models.Model):
-	cantidad = models.DecimalField(max_digits=12, decimal_places=2)
-	sucursal = models.ForeignKey(Sucursal, on_delete=models.CASCADE, null=True, blank=True)
-	start_date = models.DateField()
-	end_date = models.DateField()
-	create_at = models.DateTimeField(auto_now_add=True)
+    cantidad = models.DecimalField(max_digits=12, decimal_places=2)
+    sucursal = models.ForeignKey(
+        Sucursal, on_delete=models.CASCADE, null=True, blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    create_at = models.DateTimeField(auto_now_add=True)
