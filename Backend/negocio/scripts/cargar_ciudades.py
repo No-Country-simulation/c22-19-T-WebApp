b2070f3585ad0eba3e152e9d339267d031175c31
LeonMateoCaceres <leonmateocaceres705@gmail.com>
2024-11-26 17:40:35 -0300

    Creada la carpeta scripts, y dentro de ella está creada el script para cargar ciudades y provincias en la BD. También se añadió una extensión django_extension

---
+++ Backend/negocio/scripts/cargar_ciudades.py
@@ -0,0 +1,50 @@
+import django_extensions
+from negocio.models import Provincia, Ciudad
+
+def run():
+
+    #Defino un diccionario con las Provincias (clave) y sus ciudades (valor):
+
+    datos = {
+    "Buenos Aires": ["La Plata", "Mar del Plata", "Bahía Blanca"],
+    "Ciudad Autónoma de Buenos Aires": ["Palermo", "Recoleta", "San Telmo"],
+    "Catamarca": ["San Fernando del Valle de Catamarca", "Belén", "Tinogasta"],
+    "Chaco": ["Resistencia", "Presidencia Roque Sáenz Peña", "Villa Ángela"],
+    "Chubut": ["Rawson", "Comodoro Rivadavia", "Puerto Madryn"],
+    "Córdoba": ["Córdoba", "Villa Carlos Paz", "Río Cuarto"],
+    "Corrientes": ["Corrientes", "Goya", "Paso de los Libres"],
+    "Entre Ríos": ["Paraná", "Concordia", "Gualeguaychú"],
+    "Formosa": ["Formosa", "Clorinda", "Ingeniero Juárez"],
+    "Jujuy": ["San Salvador de Jujuy", "Humahuaca", "Tilcara"],
+    "La Pampa": ["Santa Rosa", "General Pico", "Toay"],
+    "La Rioja": ["La Rioja", "Chilecito", "Villa Unión"],
+    "Mendoza": ["Mendoza", "San Rafael", "Luján de Cuyo"],
+    "Misiones": ["Posadas", "Puerto Iguazú", "Oberá"],
+    "Neuquén": ["Neuquén", "San Martín de los Andes", "Zapala"],
+    "Río Negro": ["Viedma", "San Carlos de Bariloche", "General Roca"],
+    "Salta": ["Salta", "Cafayate", "Tartagal"],
+    "San Juan": ["San Juan", "Caucete", "Jáchal"],
+    "San Luis": ["San Luis", "Villa Mercedes", "Merlo"],
+    "Santa Cruz": ["Río Gallegos", "El Calafate", "Puerto Deseado"],
+    "Santa Fe": ["Rosario", "Santa Fe", "Rafaela"],
+    "Santiago del Estero": ["Santiago del Estero", "La Banda", "Termas de Río Hondo"],
+    "Tierra del Fuego": ["Ushuaia", "Río Grande", "Tolhuin"],
+    "Tucumán": ["San Miguel de Tucumán", "Tafí del Valle", "Concepción"],
+    }
+
+    for nombre_provincia, ciudades in datos.items():
+        provincia, created = Provincia.objects.get_or_create(nombre = nombre_provincia)
+
+        if created:
+            print(f"Provincia creada: {nombre_provincia}")
+        else:
+            print(f"La Provincia {nombre_provincia} ya existe: ")
+
+        for ciudad_nombre in ciudades:
+            ciudad, created = Ciudad.objects.get_or_create(nombre=ciudad_nombre, provincia=provincia)
+            if created:
+                print(f"Ciudad creada: {ciudad_nombre}")
+            else:
+                print(f"Ciudad ya existe: {ciudad_nombre}")
+
+    print("Carga de datos completada.")
