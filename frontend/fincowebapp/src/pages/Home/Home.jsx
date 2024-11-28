856389f87e8b78a905fb7af09e2222d02aca685a
elvin <elvindanielmolo@ufps.edu.co>
2024-11-28 12:42:11 -0500

    funcionamiento de icoe input de filtro

--- frontend/fincowebapp/src/pages/Home/Home.jsx
+++ frontend/fincowebapp/src/pages/Home/Home.jsx
@@ -3,11 +3,23 @@ import { Header, CardWelcome } from "../../components/"
 import { fakeUser } from "../../utils/data";
 import { SearchBar } from "../../components/filter/SearchBar";
 import Card_ventas_sucursales from "../../components/Card_ventas_sucursales/Card_ventas_sucursales";
-
+import { useState } from "react";
 export const Home = () => {
 
+  const [searchtText, setSearchText] = useState("");
+  const [selectecIcon, setSelectedIcon] = useState("");
 
+  const cardsData = [
+    { sucursal: "Constitución", localidad: "Colima", ventas: 700, objetivo_ventas: 1000 },
+    { sucursal: "La Toscana", localidad: "Guadalajara", ventas: 650, objetivo_ventas: 1000 },
+    { sucursal: "Las Garzas", localidad: "Monterrey", ventas: 500, objetivo_ventas: 1000 },
 
+  ];
+
+  const filteredCards =  selectecIcon === "building"
+  ? cardsData.filter((card) => 
+    card.sucursal.toLowerCase().includes(searchtText.toLowerCase())
+  ):[];
     return (
       <>
         <Header />
@@ -22,26 +34,20 @@ export const Home = () => {
             period="mensual"
           />
 
-           <SearchBar></SearchBar>
-          <Card_ventas_sucursales
-          sucursal="Constitución"
-          localidad="Colima"
-          ventas={700}
-          objetivo_ventas={1000}
-          />
-          <Card_ventas_sucursales
-          sucursal="La Toscana"
-          localidad="Guadalajara"
-          ventas={650}
-          objetivo_ventas={1000}
-          />
-          <Card_ventas_sucursales
-          sucursal="Las Garzas"
-          localidad="Monterrey"
-          ventas={500}
-          objetivo_ventas={1000}
-          />
-
+           <SearchBar 
+              setSearchText={setSearchText}
+              setSelectedIcon={setSelectedIcon} 
+           />
+          
+          {filteredCards.map((card, index) => (
+            <Card_ventas_sucursales 
+              key={index}
+              sucursal={card.sucursal}
+              localidad={card.localidad}
+              ventas={card.ventas}
+              objetivo_ventas={card.objetivo_ventas}
+            />
+          ))}
         </main>
       </>
       );
