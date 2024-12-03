15d97888c9ce8b4f4f23344a72415b508870f216
Ignacio Nava <ignan_90@hotmail.com>
2024-12-03 16:35:52 -0300

    Creacion endpoint CSRF-TOKEN y configuracion CORS

--- Backend/user/urls.py
+++ Backend/user/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('login/', views.UserLogin.as_view(), name="login"),
     path('logout/', views.UserLogout.as_view(), name="logout"),
-    path('user/', views.UserView.as_view(), name="user"), # Muestra datos del usuario
+    # Muestra datos del usuario
+    path('user/', views.UserView.as_view(), name="user"),
+    path('get-csrf/', views.csrf_token_view, name="user"),
 ]
