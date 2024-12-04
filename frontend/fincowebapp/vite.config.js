9f13ac4701200f878c77fce9ec83354c7c26b828
cesar <cesarhernanruscica@gmail.com>
2024-12-03 22:49:23 -0300

    fix recargar paginas

--- frontend/fincowebapp/vite.config.js
+++ frontend/fincowebapp/vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: mode === 'production' ? 'https://c22-19-t-webapp.onrender.com/' : '/',
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? 'https://c22-19-t-webapp.onrender.com' : '/',
+  };
+});
