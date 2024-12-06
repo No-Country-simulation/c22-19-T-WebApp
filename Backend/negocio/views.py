d1c8685dc083cf49c01146bd0a873000e749a430
Ignacio Nava <ignan_90@hotmail.com>
2024-12-06 20:21:18 -0300

    Creacion de Filtros Para APIs

--- Backend/negocio/views.py
+++ Backend/negocio/views.py
@@ -1,26 +1,29 @@
-from rest_framework import viewsets
+from .filters import ModelViewSetFiltered
+from .filters import VENTA
 from rest_framework import permissions
 
 from .models import Sucursal, Venta, Meta
 from .serializers import SucursalSerializer, VentaSerializer, MetaSerializer
 
 
-class SucursalView(viewsets.ModelViewSet):
+class SucursalView(ModelViewSetFiltered):
     serializer_class = SucursalSerializer
     permission_classes = [permissions.IsAuthenticated]
     queryset = Sucursal.objects.all()
-    http_method_names = ['get', 'post', 'put', 'patch']  # Excluye 'delete'
+    http_method_names = ['get']
 
 
-class VentaView(viewsets.ModelViewSet):
+class VentaView(ModelViewSetFiltered):
     serializer_class = VentaSerializer
-    queryset = Venta.objects.all()
-    http_method_names = ['get', 'post', 'put', 'patch']  # Excluye 'delete'
     permission_classes = [permissions.IsAuthenticated]
+    queryset = Venta.objects.all()
+    http_method_names = ['get']
+
+    filter_object = VENTA
 
 
-class MetaView(viewsets.ModelViewSet):
+class MetaView(ModelViewSetFiltered):
     serializer_class = MetaSerializer
     queryset = Meta.objects.all()
-    http_method_names = ['get', 'post', 'put', 'patch']  # Excluye 'delete'
+    http_method_names = ['get']
     permission_classes = [permissions.IsAuthenticated]
