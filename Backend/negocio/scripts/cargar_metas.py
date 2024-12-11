d5478acb9565a3f4d923213529ce661e77ec00a5
Ignacio Nava <ignan_90@hotmail.com>
2024-12-11 18:07:17 -0300

    Cambios en los print

--- Backend/negocio/scripts/cargar_metas.py
+++ Backend/negocio/scripts/cargar_metas.py
@@ -4,22 +4,24 @@ import random
 from datetime import datetime, timedelta
 
 from negocio.models import Meta, Sucursal
-
+from negocio.scripts.utils import Logger
 
 semanal_minimo = 500
 semanal_maximo = 1000
 
-def run():
-    print("Cargando metas") 
-    sucursales = Sucursal.objects.all()
-    metas_creadas = 0 # contador de las metas creadas
 
-    for sucursal in sucursales: # las metas estan calculadas de forma proporcional
+def run():
+    Logger.title('Cargando metas')
+
+    sucursales = Sucursal.objects.all()
+    metas_creadas = 0  # contador de las metas creadas
+
+    for sucursal in sucursales:  # las metas estan calculadas de forma proporcional
         semanal = round(random.uniform(semanal_minimo, semanal_maximo), 2)
         mensual = semanal * 4
         anual = mensual * 12
 
-        # crear el objeto meta para que quede asociado a la sucursal:
+        #  crear el objeto meta para que quede asociado a la sucursal:
         meta, created = Meta.objects.get_or_create(
             sucursal=sucursal,
             defaults={
@@ -30,10 +32,7 @@ def run():
         )
 
         if created:
-            metas_creadas += 1 # aumenta el contador de mis metas
-            print(f"Meta creada para la sucursal: {sucursal.nombre} "
-                  f"(Semanal: {semanal}, Mensual: {mensual}, Anual: {anual})")
-        else:
-            print(f"Meta ya existente para la sucursal: {sucursal.nombre}")
+            metas_creadas += 1  # aumenta el contador de mis metas
+            Logger.body(f"Metas: {metas_creadas}")
 
-    print(f"Total de metas creadas: {metas_creadas}")
+    Logger.body(f"Metas: {metas_creadas}", end=True)
